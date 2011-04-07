21b4f0ec43fb5568ea4e23e2f9b169aca1c1d4b5
Kevin Day <kday@mcneese.edu>
2011-04-07 15:19:20 -0500

    Bugfix: Validate variables before operating on them
    The first problem this solves is that of accessing an array item without
    validating that the item exists. In php, this produces an error. isset()
    us used here to prevent that from happending.
    
    The second problem is that if the renderred variable is empty, then do
    we display the block or not? The proper solution to this is to provide
    options in the theme settings to toggle which variable should and should
    not have its html wrapper displayed or not when the renderred variable
    is empty. At this time we need to have the renderred variable not
    displayed so html wrapper elements (such as divs) will not be produced.
    
    A better long-term solution to the second problem is to provide the
    mentioned theme settings variable empty toggle option.
    
    This is only fixed in the page.tpl.php template for now, but the other
    *.tpl.php files probably need the same or similar fix.

--- sites/all/themes/genesis/templates/page.tpl.php
+++ sites/all/themes/genesis/templates/page.tpl.php
@@ -67,54 +67,69 @@
  * @see template_preprocess_page()
  * @see template_process()
  */
+
+  // FIXME: should these be placed in the template preprocess file?
+  $leaderboard = isset($page['leaderboard']) ? render($page['leaderboard']) : '';
+  $primary_links = isset($page['primary_links']) ? render($page['primary_links']) : '';
+  $header = isset($page['header']) ? render($page['header']) : '';
+  $secondary_content = isset($page['secondary_content']) ? render($page['secondary_content']) : '';
+  $highlighted = isset($page['highlighted']) ? render($page['highlighted']) : '';
+  $help = isset($page['help']) ? render($page['help']) : '';
+  $content = isset($page['content']) ? render($page['content']) : '';
+  $sidebar_first = isset($page['sidebar_first']) ? render($page['sidebar_first']) : '';
+  $sidebar_second = isset($page['sidebar_second']) ? render($page['sidebar_second']) : '';
+  $tertiary_content = isset($page['tertiary_content']) ? render($page['tertiary_content']) : '';
+  $footer = isset($page['footer']) ? render($page['footer']) : '';
+  $renderred_tabs = isset($tabs) ? render($tabs) : '';
+  $renderred_action_links = isset($action_links) ? render($action_links) : '';
 ?>
   <?php if (!$in_overlay): // hide in overlay ?>
 
-    <?php if ($page['leaderboard']): ?>
+    <?php if (!empty($leaderboard)): ?>
       <div id="leaderboard" class="clearfix">
-        <?php print render($page['leaderboard']); ?>
+        <?php print($leaderboard); ?>
       </div>
     <?php endif; ?>
 
-    <?php if ($secondary_menu_links): ?>
+    <?php if (!empty($secondary_menu_links)): ?>
       <div id="secondary-menu-wrapper" class="clearfix">
         <div class="secondary-menu-inner"><?php print $secondary_menu_links; ?></div>
       </div>
     <?php endif; ?>
 
     <div id="header" class="clearfix">
-    <?php if (isset($page['primary_links'])) { ?>
+    <?php if (!empty($primary_links)) { ?>
       <div id='main-menu-wrapper' class='clearfix'>
-        <div class="main-menu-inner"><?php print(drupal_render($page['primary_links'])); ?></div>
+        <div class="main-menu-inner"><?php print($primary_links); ?></div>
       </div>
     <?php } ?>
 
       <?php if ($site_logo || $site_name || $site_slogan): ?>
         <div id="branding">
 
-          <?php if ($site_logo or $site_name): ?>
-            <?php if ($title): ?>
+          <?php if (!empty($site_logo) || !empty($site_name)): ?>
+            <?php if (!empty($title)): ?>
               <div class="logo-site-name"><strong>
-                <?php if ($site_logo): ?><span id="logo"><?php print $site_logo; ?></span><?php endif; ?>
-                <?php if ($site_name): ?><span id="site-name"><?php print $site_name; ?></span><?php endif; ?>
+                <?php if (!empty($site_logo)): ?><span id="logo"><?php print $site_logo; ?></span><?php endif; ?>
+                <?php if (!empty($site_name)): ?><span id="site-name"><?php print $site_name; ?></span><?php endif; ?>
               </strong></div>
             <?php else: /* Use h1 when the content title is empty */ ?>
               <h1 class="logo-site-name">
-                <?php if ($site_logo): ?><span id="logo"><?php print $site_logo; ?></span><?php endif; ?>
-                <?php if ($site_name): ?><span id="site-name"><?php print $site_name; ?></span><?php endif; ?>
+                <?php if (!empty($site_logo)): ?><span id="logo"><?php print $site_logo; ?></span><?php endif; ?>
+                <?php if (!empty($site_name)): ?><span id="site-name"><?php print $site_name; ?></span><?php endif; ?>
              </h1>
             <?php endif; ?>
           <?php endif; ?>
 
-          <?php if ($site_slogan): ?>
+          <?php if (!empty($site_slogan)): ?>
             <div id="site-slogan"><?php print $site_slogan; ?></div>
           <?php endif; ?>
 
         </div> <!-- /branding -->
       <?php endif; ?>
 
-      <?php if ($page['header']): ?>
-        <div id="header-blocks"><?php print render($page['header']); ?></div>
+      <?php if (!empty($header)): ?>
+        <div id="header-blocks"><?php print($header); ?></div>
       <?php endif; ?>
 
     </div> <!-- /header -->
@@ -124,11 +139,11 @@
 
   <?php print $breadcrumb; ?>
   <?php print $messages; ?>
-  <?php print render($page['help']); ?>
+  <?php if (!empty($help)) print($help); ?>
 
-  <?php if ($page['secondary_content'] && !$in_overlay): // hide in overlay ?>
+  <?php if (!empty($secondary_content) && !$in_overlay): // hide in overlay ?>
     <div id="secondary-content">
-      <?php print render($page['secondary_content']); ?>
+      <?php print($secondary_content); ?>
     </div>
   <?php endif; ?>
 
@@ -136,54 +151,54 @@
     <div id="content-column">
       <div class="content-inner">
 
-        <?php if ($page['highlighted']): ?>
-          <div id="highlighted"><?php print render($page['highlighted']); ?></div>
+        <?php if (!empty($highlighted)): ?>
+          <div id="highlighted"><?php print($highlighted); ?></div>
         <?php endif; ?>
 
         <div id="main-content">
           <?php print render($title_prefix); ?>
-          <?php if ($title): ?>
+          <?php if (!empty($title)): ?>
             <h1 id="page-title"><?php print $title; ?></h1>
           <?php endif; ?>
           <?php print render($title_suffix); ?>
 
-          <?php if ($tabs): ?>
-            <div class="local-tasks"><?php print render($tabs); ?></div>
+          <?php if (!empty($renderred_tabs)): ?>
+            <div class="local-tasks"><?php print($renderred_tabs); ?></div>
           <?php endif; ?>
 
-          <?php if ($action_links): ?>
-            <ul class="action-links"><?php print render($action_links); ?></ul>
+          <?php if (!empty($renderred_action_links)): ?>
+            <ul class="action-links"><?php print($renderred_action_links); ?></ul>
           <?php endif; ?>
 
           <div id="content">
-            <?php print render($page['content']); ?>
+            <?php if (!empty($content)) print($content); ?>
           </div>
         </div>
 
       </div>
     </div>
 
-    <?php if ($page['sidebar_first']): ?>
-      <div id="sidebar-first" class="sidebar"><?php print render($page['sidebar_first']); ?></div>
+    <?php if (!empty($sidebar_first)): ?>
+      <div id="sidebar-first" class="sidebar"><?php print($sidebar_first); ?></div>
     <?php endif; ?>
 
-    <?php if ($page['sidebar_second']): ?>
-      <div id="sidebar-second" class="sidebar"><?php print render($page['sidebar_second']); ?></div>
+    <?php if (!empty($sidebar_second)): ?>
+      <div id="sidebar-second" class="sidebar"><?php print($sidebar_second); ?></div>
     <?php endif; ?>
 
   </div> <!-- /columns -->
 
   <?php if (!$in_overlay): // hide in overlay ?>
 
-    <?php if ($page['tertiary_content']): ?>
+    <?php if (!empty($tertiary_content)): ?>
       <div id="tertiary-content">
-        <?php print render($page['tertiary_content']); ?>
+        <?php print($tertiary_content); ?>
       </div>
     <?php endif; ?>
 
-    <?php if ($page['footer'] || $feed_icons): ?>
+    <?php if (!empty($footer) || $feed_icons): ?>
       <div id="footer">
-        <?php print render($page['footer']); ?>
+        <?php print render($footer); ?>
         <?php print $feed_icons; ?>
       </div>
     <?php endif; ?>
