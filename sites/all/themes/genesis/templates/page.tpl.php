ade9d005305dc1fae3466d32127848f0e3854cb0
patrick <peustis@mcneese.edu>
2011-04-07 10:48:38 -0500

    flushed press release columns.

--- sites/all/themes/genesis/templates/page.tpl.php
+++ sites/all/themes/genesis/templates/page.tpl.php
@@ -141,11 +141,7 @@
         <?php endif; ?>
 
         <div id="main-content">
-          <?php print render($title_prefix); ?>
-          <?php if ($title): ?>
-            <h1 id="page-title"><?php print $title; ?></h1>
-          <?php endif; ?>
-          <?php print render($title_suffix); ?>
+
 
           <?php if ($tabs): ?>
             <div class="local-tasks"><?php print render($tabs); ?></div>
