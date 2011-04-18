e25d619bb94ee645d6b0cc2f8a6521c31081845c
Kevin Day <kday@mcneese.edu>
2011-04-18 16:30:49 -0500

    Bugfix: Ensure that the title and similar are under main content
    This was accidentally moved too far out and ended up outside of main content.
    The end result was the skip to main content not working properly.

--- sites/all/themes/genesis/genesis_mcneese/page.tpl.php
+++ sites/all/themes/genesis/genesis_mcneese/page.tpl.php
@@ -162,18 +162,6 @@
         <ul class="action-links"><?php print render($action_links); ?></ul>
       <?php endif; ?>
 
-      <?php print render($title_prefix); ?>
-      <?php if ($title): ?>
-        <h1 id="page-title"><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-
-      <?php if ($page['subboard']): ?>
-        <div id="subboard"><?php print render($page['subboard']); ?></div>
-      <?php endif; ?>
-
-      <?php print $breadcrumb; ?>
-
       <div class="content-inner">
 
         <?php if ($page['highlighted']): ?>
@@ -181,6 +169,17 @@
         <?php endif; ?>
 
         <div id="main-content">
+          <?php print render($title_prefix); ?>
+          <?php if ($title): ?>
+            <h1 id="page-title"><?php print $title; ?></h1>
+          <?php endif; ?>
+          <?php print render($title_suffix); ?>
+
+          <?php if ($page['subboard']): ?>
+            <div id="subboard"><?php print render($page['subboard']); ?></div>
+          <?php endif; ?>
+
+          <?php print $breadcrumb; ?>
 
           <div id="content">
             <?php print render($page['content']); ?>
