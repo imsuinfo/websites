416c6b470a5620bb8bfa140cabaf673c42a27b65
Kevin Day <kday@mcneese.edu>
2011-05-27 09:43:21 -0500

    Workaround: some <div> blocks are vanishing
    I noticed in my recent browser tests that the Skip to main content link started appearing in different browsers without being selected.
    After testing, I discovered that there the parent <div> is completely vanishing out of the html on non-mozilla based browser.
    It appears that there is something on the system specifically altering the HTML before it makes it to the browser or the browsers are altering the html themselves.
    My workaround is to wrap the div inside of a span which then causes the span to vanish.

--- sites/all/themes/genesis/genesis_mcneese/html.tpl.php
+++ sites/all/themes/genesis/genesis_mcneese/html.tpl.php
@@ -48,9 +48,11 @@
 <body id="genesis_mcneese" <?php print $attributes;?> class="<?php print($emergency['css']); ?>"
 
   <?php if (!$in_overlay): // Hide the skip-link in overlay ?>
-    <div id="skip-link">
-      <a href="#content-column"><?php print t('Skip to main content'); ?></a>
-    </div>
+    <span>
+      <div id="skip-link">
+        <a href="#content-column"><?php print t('Skip to main content'); ?></a>
+      </div>
+    </span>
   <?php endif; ?>
 
   <?php print $page_top; ?>
