1f0ba18fe9182e1d27a9f52a17be9d6546193608
Kevin Day <kday@mcneese.edu>
2011-06-03 12:54:31 -0500

    Update: The maintenance page was out of date.

--- sites/all/themes/mcneese_drupal/maintenance-page.tpl.php
+++ sites/all/themes/mcneese_drupal/maintenance-page.tpl.php
@@ -1,7 +1,7 @@
-<?php?><!DOCTYPE html>
-<html lang="<?php print($language->language); ?>" dir="<?php print $language->dir; ?>">
+<?php global $base_dir; ?><!DOCTYPE html PUBLIC "-//W3C//DTD HTML+RDFa 1.1//EN">
+<html lang="<?php print($language->language); ?>" dir="<?php print $language->dir; ?>" version="HTML+RDFa 1.1" <?php print $rdf_namespaces; ?>>
 
-<head>
+<head profile="<?php print $grddl_profile; ?>">
   <!--(begin_head)-->
   <?php print($head); ?>
   <title><?php print($head_title); ?></title>
@@ -9,7 +9,16 @@
   <?php print($scripts);?>
   <!--(end_head)-->
 </head>
-<body id="mcneese_drupal-body" class="<?php print($classes); ?>" <?php print($attributes);?>>
+<body id="mcneese_drupal-body" class="<?php print($classes); print($in_overlay_css); ?>" <?php print($attributes);?>>
+<?php  if (!isset($in_overlay) || $in_overlay != 'child'){ ?><div id="mcneese_drupal-skip_nav" class="clearfix mcneese_drupal-text_shadow">
+    <!--(begin_skipnav)-->
+    <div id="mcneese_drupal-skip_nav-list">
+      <div id="mcneese_drupal-skip_nav-list-content"><a id="mcneese_drupal-skip_nav-list-content-link" class="mcneese_drupal-skipnav-link element-invisible element-focusable" href="#mcneese_drupal-content"><?php print t("Skip to main content"); ?></a></div>
+      <?php if (isset($search)){ ?><div id="mcneese_drupal-skip_nav-list-search"><a id="mcneese_drupal-skip_nav-list-search-link" class="mcneese_drupal-skipnav-link element-invisible element-focusable" href="#mcneese_drupal-search"><?php print t("Skip to search"); ?></a></div><?php } ?>
+    </div>
+    <!--(end_skipnav)-->
+  </div><?php } ?>
+
   <!--(begin_body)-->
   <div id="mcneese_drupal-page_top" class="mcneese_drupal">
     <!--(begin_page_top)-->
@@ -19,19 +28,49 @@
 
   <div id="mcneese_drupal-page" class="mcneese_drupal">
     <!--(begin_page)-->
-    <div id='mcneese_drupal-title_region' class='clearfix page-title mcneese_drupal-text_shadow' style='<?php if (isset($title_logo_style) && is_string($title_logo_style)) print($title_logo_style);?>'>
-      <div id='mcneese_drupal-title'>
-        <!--(begin_title)-->
-        <?php print(render($title_prefix)); ?>
-        <?php if (isset($site_name) && is_string($site_name)){ ?>
-          <div id='mcneese_drupal-website_title' class='mcneese_drupal-text_shadow'>
-            <?php print($site_name); ?>
+    <div id='mcneese_drupal-header_region' class='clearfix page-header mcneese_drupal-text_shadow'>
+      <div id='mcneese_drupal-header'>
+        <!--(begin_header)-->
+        <?php if (isset($logo) && !empty($logo)){ ?>
+          <div id='mcneese_drupal-website_logo' class='mcneese_drupal-text_shadow'>
+            <!--(begin_website_logo)-->
+            <a id="mcneese_drupal-website_logo-link" href="<?php print($base_dir . '/'); ?>" title="Home Page">
+              <img id="mcneese_drupal-website_logo-image" src="<?php print($logo); ?>" alt="<?php if (isset($site_name) && !empty($site_name)) print($site_name); ?>">
+            </a>
+            <!--(end_website_logo)-->
           </div>
         <?php } ?>
+        <!--(end_header)-->
+      </div>
+
+      <div id="mcneese_drupal-header-horizontal_ruler"></div>
+    </div>
+    <div id='mcneese_drupal-message_region' class='clearfix mcneese_drupal-text_shadow'>
+      <?php if (isset($messages) && is_string($messages)){ ?>
+        <!--(begin_messages)-->
+        <div id='mcneese_drupal-messages' class='clearfix'>
+          <h2 class='element-invisible mcneese_drupal-text_shadow'><?php print t("Messages"); ?></h2>
+          <?php print($messages); ?>
+        </div>
+        <!--(end_messages)-->
+      <?php } ?>
+
+      <?php if (!empty($help)){ ?>
+        <!--(begin_help)-->
+        <div id='mcneese_drupal-help' class='clearfix mcneese_drupal-text_shadow'>
+          <h2 class='element-invisible'><?php print t("Help"); ?></h2>
+          <?php print($help); ?>
+        </div>
+        <!--(end_help)-->
+      <?php } ?>
+    </div>
+
+    <div id='mcneese_drupal-title_region' class='clearfix page-title mcneese_drupal-text_shadow'>
+      <div id='mcneese_drupal-title'>
+        <!--(begin_title)-->
         <?php if (isset($title) && is_string($title)){ ?>
           <h1 id='mcneese_drupal-page_title' class='mcneese_drupal-text_shadow'><?php print($title); ?></h1>
         <?php } ?>
-        <?php print(render($title_suffix)); ?>
         <!--(end_title)-->
       </div>
 
@@ -43,38 +82,51 @@
           <!--(end_side_links)-->
         </div>
       <?php } ?>
-    </div>
 
-    <div id='mcneese_drupal-message_region' class='clearfix mcneese_drupal-text_shadow'>
-      <?php if (isset($messages) && is_string($messages)){ ?>
-        <!--(begin_messages)-->
-        <div id='mcneese_drupal-messages' class='clearfix'>
-          <h2 class='element-invisible mcneese_drupal-text_shadow'><?php print t('Messages'); ?></h2>
-          <?php print($messages); ?>
+      <?php if (is_array($primary_local_tasks)){ ?>
+        <div id='mcneese_drupal-primary_tabs'>
+          <!--(begin_primary_tabs)-->
+          <h2 class='element-invisible'><?php print t("Primary Tabs"); ?></h2>
+          <ul class='tabs primary mcneese_drupal-text_shadow'><?php print render($primary_local_tasks); ?></ul>
+          <!--(end_primary_tabs)-->
         </div>
-        <!--(end_messages)-->
       <?php } ?>
 
-      <?php if ($help){ ?>
-        <!--(begin_help)-->
-        <div id='mcneese_drupal-help' class='clearfix mcneese_drupal-text_shadow'>
-          <h2 class='element-invisible'><?php print t("Help"); ?></h2>
-          <?php print($help); ?>
+      <?php if (!empty($breadcrumb)){ ?>
+        <div id="mcneese_drupal-breadcrumb">
+          <?php print $breadcrumb; ?>
         </div>
-        <!--(end_help)-->
       <?php } ?>
     </div>
 
     <div id='mcneese_drupal-content_region' class='page-title mcneese_drupal-text_shadow'>
-      <?php if ($content){ ?>
-        <div id='mcneese_drupal-content' class='clearfix'>
-          <!--(begin_content)-->
-          <h2 class='element-invisible'><?php print(t("Primary Content")); ?></h2>
-          <?php print($content); ?>
-          <!--(end_content)-->
+      <?php if ($secondary_local_tasks){ ?>
+        <div id='mcneese_drupal-secondary_tabs'>
+          <!--(begin_secondary_tabs)-->
+          <h2 class='element-invisible'><?php print(t("Secondary Tabs")); ?></h2>
+          <ul class='tabs secondary'><?php print(render($secondary_local_tasks)); ?></ul>
+          <!--(end_secondary_tabs)-->
         </div>
       <?php } ?>
+
+      <?php if ($action_links){ ?>
+        <div id='mcneese_drupal-action_links'>
+          <!--(begin_action_links)-->
+          <h2 class='element-invisible'><?php print(t("Action Links")); ?></h2>
+          <ul class="action-links"><?php print render($action_links); ?></ul>
+          <!--(end_action_links)-->
+        </div>
+      <?php } ?>
+
+      <div id='mcneese_drupal-content' class='clearfix'>
+        <!--(begin_content)-->
+        <h2 class='element-invisible'><?php print(t("Primary Content")); ?></h2>
+        <?php print($content); ?>
+        <!--(end_content)-->
+      </div>
     </div>
+
+    <div id="mcneese_drupal-content_bottom"></div>
 
     <div id="mcneese_drupal-undercontent">
       <!--(begin_undercontent)-->
