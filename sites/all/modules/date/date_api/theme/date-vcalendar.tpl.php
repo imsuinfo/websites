12bc92d21cb1d19c919c10616452ab2c684884d9
Kevin Day <kday@mcneese.edu>
2011-08-23 09:59:37 -0500

    Update Module: date 7.x-2.0-alpha4

--- sites/all/modules/date/date_api/theme/date-vcalendar.tpl.php
+++ sites/all/modules/date/date_api/theme/date-vcalendar.tpl.php
@@ -7,11 +7,16 @@
  *   @see date-vevent.tpl.php.
  *   @see date-valarm.tpl.php.
  */
+if (empty($method)) {
+  $method = 'PUBLISH';
+}
 ?>
 BEGIN:VCALENDAR
 VERSION:2.0
-METHOD:PUBLISH
-X-WR-CALNAME: <?php print $calname ?>
+METHOD:<?php print $method; ?>
+<?php if !empty($calname): ?>
+X-WR-CALNAME;VALUE=TEXT:<?php print $calname . "\r\n"; ?>
+<?php endif; ?>
 PRODID:-//Drupal iCal API//EN
 <?php foreach($events as $event): ?>
 <?php print theme('date_vevent', $event); ?>
