54b9b80c55fee7a72de89aba253edee4d157620e
Kevin Day <kday@mcneese.edu>
2012-01-23 08:42:59 -0600

    Update Library: htmlpurifier 4.4.0

--- sites/all/libraries/htmlpurifier/library/HTMLPurifier/HTMLModule/Tables.php
+++ sites/all/libraries/htmlpurifier/library/HTMLPurifier/HTMLModule/Tables.php
@@ -37,7 +37,9 @@ class HTMLPurifier_HTMLModule_Tables extends HTMLPurifier_HTMLModule
                 'abbr'    => 'Text',
                 'colspan' => 'Number',
                 'rowspan' => 'Number',
-                'scope'   => 'Text',
+                // Apparently, as of HTML5 this attribute only applies
+                // to 'th' elements.
+                'scope'   => 'Enum#row,col,rowgroup,colgroup',
             ),
             $cell_align
         );
