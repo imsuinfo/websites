0fdecf70acbcdb2c4a7cec450a0f5c99837d71ec
Kevin Day <kday@mcneese.edu>
2012-01-27 08:50:07 -0600

    Bugfix: rss feed xml header should end its line
    For some reason the first newline after the rss header gets stripped away.
    By adding a second newline, the xml header seems to get its newline terminated properly.

--- sites/all/modules/views/theme/views-view-rss.tpl.php
+++ sites/all/modules/views/theme/views-view-rss.tpl.php
@@ -7,6 +7,7 @@
  */
 ?>
 <?php print "<?xml"; ?> version="1.0" encoding="utf-8" <?php print "?>"; ?>
+
 <rss version="2.0" xml:base="<?php print $link; ?>"<?php print $namespaces; ?>>
   <channel>
     <title><?php print $title; ?></title>
