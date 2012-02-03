013a9f87c8f194dc4cb4ecd44470aefddbd0d48d
Kevin Day <kday@mcneese.edu>
2012-02-03 09:59:27 -0600

    Update Core: drupal 7.12

--- update.php
+++ update.php
@@ -152,7 +152,6 @@ function update_helpful_links() {
 function update_results_page() {
   drupal_set_title('Drupal database update');
   $links = update_helpful_links();
-  $output = '';
 
   update_task_list();
   // Report end result.
@@ -163,10 +162,10 @@ function update_results_page() {
     $log_message = ' All errors have been logged.';
   }
 
-  if (isset($_SESSION['update_success']) && $_SESSION['update_success']) {
+  if ($_SESSION['update_success']) {
     $output = '<p>Updates were attempted. If you see no failures below, you may proceed happily to the <a href="' . base_path() . '?q=admin">administration pages</a>. Otherwise, you may need to update your database manually.' . $log_message . '</p>';
   }
-  else if (!empty($_SESSION['updates_remaining'])) {
+  else {
     list($module, $version) = array_pop(reset($_SESSION['updates_remaining']));
     $output = '<p class="error">The update process was aborted prematurely while running <strong>update #' . $version . ' in ' . $module . '.module</strong>.' . $log_message;
     if (module_exists('dblog')) {
