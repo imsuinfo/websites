e6c7f1632fd5b0c110b12a740166f244b67307c1
Kevin Day <kday@mcneese.edu>
2012-02-03 13:05:09 -0600

    Bugfix: The <pre> tag is not "tabular data'
    <pre> is not tabular data, so do not treat it as such.
    
    Its clear that it is assuming that if <pre> exist, then the data could be mis-used.
    However, <pre> is used when any literal formatting is needed to be display.
    This will produce false positives far more than it should, so remove the test.
    
    see: http://www.w3schools.com/tags/tag_pre.asp
    see: http://www.w3schools.com/html5/tag_pre.asp

--- sites/all/libraries/quail/quail/common/accessibility_tests.php
+++ sites/all/libraries/quail/quail/common/accessibility_tests.php
@@ -5773,7 +5773,7 @@ class tabularDataIsInTable extends quailTest {
 	*/
 	function check() {
 		foreach($this->getAllElements(null, 'text') as $text) {
-			if(strpos($text->nodeValue, "\t") !== false || $text->tagName == 'pre')
+			if(strpos($text->nodeValue, "\t") !== false)
 				$this->addReport($text);
 		}
 	}
