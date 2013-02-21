de3edab14934134d09091fb9d504b65f26bade56
Kevin Day <kday@mcneese.edu>
2013-02-21 14:48:55 -0600

    Update Module: webform_accordion 7.x-1.1

--- sites/all/modules/webform_accordion/js/webform_accordion.js
+++ sites/all/modules/webform_accordion/js/webform_accordion.js
@@ -6,7 +6,14 @@
 (function ($) {
   Drupal.behaviors.webform_accordion = {
     attach: function(context) {
-      $('.accordion-container').accordion()
+
+      for (var id in Drupal.settings.webform_accordion) {
+        var options = {}
+        for (var option_name in Drupal.settings.webform_accordion[id]) {
+          options[option_name] = Drupal.settings.webform_accordion[id][option_name];
+        }
+        $('#' + id).accordion(options);
+      }
     }
   }
 })(jQuery);
