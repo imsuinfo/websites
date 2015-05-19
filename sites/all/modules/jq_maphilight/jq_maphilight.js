8c4eda784a260998309dcb7decdb642e82004369
Kevin Day <kday@mcneese.edu>
2015-05-19 14:36:36 -0500

    Bugfix: (jq_maphilight) provide defaults when settings.jq_maphilight is undefined

--- sites/all/modules/jq_maphilight/jq_maphilight.js
+++ sites/all/modules/jq_maphilight/jq_maphilight.js
@@ -2,19 +2,19 @@
   Drupal.behaviors.jq_maphilight = {
     attach: function(context, settings) {
       var options = {
-        fill: (settings.jq_maphilight.fill == 'true' ? true : false),
-        fillColor: settings.jq_maphilight.fillColor,
-        fillOpacity: settings.jq_maphilight.fillOpacity,
-        stroke: (settings.jq_maphilight.stroke == 'true' ? true : false),
-        strokeColor: settings.jq_maphilight.strokeColor,
-        strokeOpacity: settings.jq_maphilight.strokeOpacity,
-        strokeWidth: settings.jq_maphilight.strokeWidth,
-        fade: (settings.jq_maphilight.fade == 'true' ? true : false),
-        alwaysOn: (settings.jq_maphilight.alwaysOn == 'true' ? true : false),
-        neverOn: (settings.jq_maphilight.neverOn == 'true' ? true : false),
-        groupBy: (settings.jq_maphilight.groupBy == 'true' ? true : false)
+        fill: (settings.jq_maphilight && settings.jq_maphilight.fill == 'true' ? true : false),
+        fillColor: settings.jq_maphilight && settings.jq_maphilight.fillColor ? settings.jq_maphilight.fillColor : '000000',
+        fillOpacity: settings.jq_maphilight && settings.jq_maphilight.fillOpacity ? settings.jq_maphilight.fillOpacity : 1 / 10,
+        stroke: (settings.jq_maphilight && settings.jq_maphilight.stroke == 'true' ? true : false),
+        strokeColor: settings.jq_maphilight && settings.jq_maphilight.strokeColor ? settings.jq_maphilight.strokeColor : 'ff0000',
+        strokeOpacity: settings.jq_maphilight && settings.jq_maphilight.strokeOpacity ? settings.jq_maphilight.strokeOpacity : 1,
+        strokeWidth: settings.jq_maphilight && settings.jq_maphilight.strokeWidth ? settings.jq_maphilight.strokeWidth : 2,
+        fade: (settings.jq_maphilight && settings.jq_maphilight.fade == 'true' ? true : false),
+        alwaysOn: (settings.jq_maphilight && settings.jq_maphilight.alwaysOn == 'true' ? true : false),
+        neverOn: (settings.jq_maphilight && settings.jq_maphilight.neverOn == 'true' ? true : false),
+        groupBy: (settings.jq_maphilight && settings.jq_maphilight.groupBy == 'true' ? true : false)
       }
-  if (settings.jq_maphilight.allMapsEnabled  == 'true') {
+  if (settings.jq_maphilight && settings.jq_maphilight.allMapsEnabled  == 'true') {
     $('img[usemap]').maphilight(options);
   }
   else {
