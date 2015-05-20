8d269b774233eb8c08e3c9df35e9c174b6193815
Kevin Day <kday@mcneese.edu>
2015-05-20 08:46:24 -0500

    Update: provide mcneese-favorable default styles when settings.jq_maphilight is not available

--- sites/all/modules/jq_maphilight/jq_maphilight.js
+++ sites/all/modules/jq_maphilight/jq_maphilight.js
@@ -2,11 +2,11 @@
   Drupal.behaviors.jq_maphilight = {
     attach: function(context, settings) {
       var options = {
-        fill: (settings.jq_maphilight && settings.jq_maphilight.fill == 'true' ? true : false),
+        fill: (settings.jq_maphilight && settings.jq_maphilight.fill == 'true' ? true : true),
         fillColor: settings.jq_maphilight && settings.jq_maphilight.fillColor ? settings.jq_maphilight.fillColor : '000000',
-        fillOpacity: settings.jq_maphilight && settings.jq_maphilight.fillOpacity ? settings.jq_maphilight.fillOpacity : 1 / 10,
-        stroke: (settings.jq_maphilight && settings.jq_maphilight.stroke == 'true' ? true : false),
-        strokeColor: settings.jq_maphilight && settings.jq_maphilight.strokeColor ? settings.jq_maphilight.strokeColor : 'ff0000',
+        fillOpacity: settings.jq_maphilight && settings.jq_maphilight.fillOpacity ? settings.jq_maphilight.fillOpacity : 0.18,
+        stroke: (settings.jq_maphilight && settings.jq_maphilight.stroke == 'true' ? true : true),
+        strokeColor: settings.jq_maphilight && settings.jq_maphilight.strokeColor ? settings.jq_maphilight.strokeColor : 'fad704',
         strokeOpacity: settings.jq_maphilight && settings.jq_maphilight.strokeOpacity ? settings.jq_maphilight.strokeOpacity : 1,
         strokeWidth: settings.jq_maphilight && settings.jq_maphilight.strokeWidth ? settings.jq_maphilight.strokeWidth : 2,
         fade: (settings.jq_maphilight && settings.jq_maphilight.fade == 'true' ? true : false),
