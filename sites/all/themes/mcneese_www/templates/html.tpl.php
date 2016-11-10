6ea62aa536580b92101f01c6b27aedd22fb8db63
Kevin Day <kday@mcneese.edu>
2016-11-10 09:02:32 -0600

    Update: add <base> markup tag to hint at relative url absolute base url

--- sites/all/themes/mcneese_www/templates/html.tpl.php
+++ sites/all/themes/mcneese_www/templates/html.tpl.php
@@ -4,6 +4,7 @@
  * Html theme implementation.
  */
   $cf = & drupal_static('cf_theme_get_variables', array());
+  global $base_url;
 
   if (!isset($cf['generic']['tags'])) {
     mcneese_initialize_generic_tags($cf);
@@ -26,6 +27,7 @@
   print('>');
   print('<head>');
   print('<!--(begin-head)-->');
+  print('<base href="' . $base_url . '">');
   print($head . "\n");
   print($cf['headers'] . "\n");
   print('<title>');
