fbf808e32e717debb263c62585a7cbd656b360a8
Kevin Day <kday@mcneese.edu>
2016-12-12 10:38:23 -0600

    Bugfix: add php7 support to icalcreator
    see: https://www.drupal.org/node/2707373
    see: https://www.drupal.org/files/issues/iCalcreator-php-7-2707373-6.patch

--- sites/all/libraries/iCalcreator/iCalcreator.class.php
+++ sites/all/libraries/iCalcreator/iCalcreator.class.php
@@ -73,7 +73,7 @@ class vcalendar {
  * @param array $config
  * @return void
  */
-  function vcalendar ( $config = array()) {
+  function __construct( $config = array()) {
     $this->_makeVersion();
     $this->calscale   = null;
     $this->method     = null;
@@ -1958,7 +1958,7 @@ class calendarComponent {
  * @author Kjell-Inge Gustafsson, kigkonsult <ical@kigkonsult.se>
  * @since 2.9.6 - 2011-05-17
  */
-  function calendarComponent() {
+  function __construct() {
     $this->objName         = ( isset( $this->timezonetype )) ?
                           strtolower( $this->timezonetype )  :  get_class ( $this );
     $this->uid             = array();
@@ -6219,8 +6219,8 @@ class vevent extends calendarComponent {
  * @param  array $config
  * @return void
  */
-  function vevent( $config = array()) {
-    $this->calendarComponent();
+  function __construct( $config = array()) {
+    parent::__construct();
 
     $this->attach          = '';
     $this->attendee        = '';
@@ -6366,8 +6366,8 @@ class vtodo extends calendarComponent {
  * @param array $config
  * @return void
  */
-  function vtodo( $config = array()) {
-    $this->calendarComponent();
+  function __construct( $config = array()) {
+    parent::__construct();
 
     $this->attach          = '';
     $this->attendee        = '';
@@ -6505,8 +6505,8 @@ class vjournal extends calendarComponent {
  * @param array $config
  * @return void
  */
-  function vjournal( $config = array()) {
-    $this->calendarComponent();
+  function __construct( $config = array()) {
+    parent::__construct();
 
     $this->attach          = '';
     $this->attendee        = '';
@@ -6615,8 +6615,8 @@ class vfreebusy extends calendarComponent {
  * @param array $config
  * @return void
  */
-  function vfreebusy( $config = array()) {
-    $this->calendarComponent();
+  function __construct( $config = array()) {
+    parent::__construct();
 
     $this->attendee        = '';
     $this->comment         = '';
@@ -6697,8 +6697,8 @@ class valarm extends calendarComponent {
  * @param array $config
  * @return void
  */
-  function valarm( $config = array()) {
-    $this->calendarComponent();
+  function __construct( $config = array()) {
+    parent::__construct();
 
     $this->action          = '';
     $this->attach          = '';
@@ -6780,7 +6780,7 @@ class vtimezone extends calendarComponent {
  * @param array $config
  * @return void
  */
-  function vtimezone( $timezonetype=FALSE, $config = array()) {
+  function __construct( $timezonetype=FALSE, $config = array()) {
     if( is_array( $timezonetype )) {
       $config       = $timezonetype;
       $timezonetype = FALSE;
@@ -6789,7 +6789,7 @@ class vtimezone extends calendarComponent {
       $this->timezonetype = 'VTIMEZONE';
     else
       $this->timezonetype = strtoupper( $timezonetype );
-    $this->calendarComponent();
+    parent::__construct();
 
     $this->comment         = '';
     $this->dtstart         = '';
