c2912460a0897d17990e7caa2dcc3b00f00a7c14
Kevin Day <kday@mcneese.edu>
2017-06-28 14:38:09 -0500

    Bugfix: add inline-block <div> tags to complaint log title and add spaces
    This makes it so that text-only code will be displayed under a new line for the title while preserving the side-by-side look that is the displayed in markup.
    Spaces are added to preserve readability in text mode (this change should not be visually noticable).
    
    Signed-off-by: Kevin Day <kday@mcneese.edu>

--- sites/all/themes/mcneese_www/templates/node--university_police_complaint_log.tpl.php
+++ sites/all/themes/mcneese_www/templates/node--university_police_complaint_log.tpl.php
@@ -24,27 +24,27 @@
   $occurred_date = date("m/d/Y - h:ia", $occurred_date_stamp);
 
   print('<div id="police_complaint_log-entry-print($nid); " class="police_complaint_log-entry">');
-  print('<h2 class="police_complaint_log-title">Complaint Number:');
-  print($title);
+  print('<h2 class="police_complaint_log-title"><div class="inline-block margin-right-10">Complaint Number: </div>');
+  print('<div class="inline-block">' . $title . '</div>');
   print('</h2>');
 
   print('<div class="police_complaint_log-block">');
   print('<div class="police_complaint_log-item police_complaint_log-item-odd police_complaint_log-nature_of_complaint">');
-  print('<h3 class="police_complaint_log-item-label police_complaint_log-nature_of_complaint-label"><strong>Nature of Complaint:</strong></h3>');
+  print('<h3 class="police_complaint_log-item-label police_complaint_log-nature_of_complaint-label"><strong>Nature of Complaint: </strong></h3>');
   print('<div class="police_complaint_log-item-value police_complaint_log-nature_of_complaint-value">');
   print($node->field_police_nat_of_comp['und'][0]['safe_value']);
   print('</div>');
   print('</div>');
 
   print('<div class="police_complaint_log-item police_complaint_log-item-even police_complaint_log-disposition">');
-  print('<h3 class="police_complaint_log-item-label police_complaint_log-disposition-label"><strong>Disposition:</strong></h3>');
+  print('<h3 class="police_complaint_log-item-label police_complaint_log-disposition-label"><strong>Disposition: </strong></h3>');
   print('<div class="police_complaint_log-item-value police_complaint_log-disposition-value">');
   print($node->field_police_disposition['und'][0]['safe_value']);
   print('</div>');
   print('</div>');
 
   print('<div class="police_complaint_log-item police_complaint_log-item-odd police_complaint_log-date">');
-  print('<h3 class="police_complaint_log-item-label police_complaint_log-reported-date-label"><strong>Reported Date:</strong></h3>');
+  print('<h3 class="police_complaint_log-item-label police_complaint_log-reported-date-label"><strong>Reported Date: </strong></h3>');
   print('<div class="police_complaint_log-item-value police_complaint_log-reported-date-value">');
   print($police_date);
   print('</div>');
@@ -52,34 +52,34 @@
 
   if (!is_null($occurred_date_stamp)) {
     print('<div class="police_complaint_log-item police_complaint_log-item-even police_complaint_log-date">');
-    print('<h3 class="police_complaint_log-item-label police_complaint_log-ocurred-date-label"><strong>Occurred Date:</strong></h3>');
+    print('<h3 class="police_complaint_log-item-label police_complaint_log-ocurred-date-label"><strong>Occurred Date: </strong></h3>');
     print('<div class="police_complaint_log-item-value police_complaint_log-ocurred-date-value">');
     print($occurred_date);
     print('</div>');
     print('</div>');
 
     print('<div class="police_complaint_log-item police_complaint_log-item-odd police_complaint_log-offense_location">');
-    print('<h3 class="police_complaint_log-item-label police_complaint_log-offense_location-label"><strong>Offense Location:</strong></h3>');
+    print('<h3 class="police_complaint_log-item-label police_complaint_log-offense_location-label"><strong>Offense Location: </strong></h3>');
     print('<div class="police_complaint_log-item-value police_complaint_log-offense_location-value">');
     print($node->field_police_off_loc['und'][0]['safe_value']);
     print('</div>');
     print('</div>');
 
     print('<div class="police_complaint_log-item police_complaint_log-item-even police_complaint_log-synopsis">');
-    print('<h3 class="police_complaint_log-item-label police_complaint_log-synopsis-label"><strong>Synopsis:</strong></h3>');
+    print('<h3 class="police_complaint_log-item-label police_complaint_log-synopsis-label"><strong>Synopsis: </strong></h3>');
     print('<div class="police_complaint_log-item-value police_complaint_log-synopsis-value">');
     print($node->field_police_synopsis['und'][0]['safe_value']);
   }
   else {
     print('<div class="police_complaint_log-item police_complaint_log-item-even police_complaint_log-offense_location">');
-    print('<h3 class="police_complaint_log-item-label police_complaint_log-offense_location-label"><strong>Offense Location:</strong></h3>');
+    print('<h3 class="police_complaint_log-item-label police_complaint_log-offense_location-label"><strong>Offense Location: </strong></h3>');
     print('<div class="police_complaint_log-item-value police_complaint_log-offense_location-value">');
     print($node->field_police_off_loc['und'][0]['safe_value']);
     print('</div>');
     print('</div>');
 
     print('<div class="police_complaint_log-item police_complaint_log-item-odd police_complaint_log-synopsis">');
-    print('<h3 class="police_complaint_log-item-label police_complaint_log-synopsis-label"><strong>Synopsis:</strong></h3>');
+    print('<h3 class="police_complaint_log-item-label police_complaint_log-synopsis-label"><strong>Synopsis: </strong></h3>');
     print('<div class="police_complaint_log-item-value police_complaint_log-synopsis-value">');
     print($node->field_police_synopsis['und'][0]['safe_value']);
   }
